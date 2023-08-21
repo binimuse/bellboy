ec8bd9b4705e1d862fe6efc234af78ce776d3d81
binimuse <bini.musema@gmail.com>
2023-08-21 16:05:13 +0300

    <fix> main page fixed (setup for the project done)

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,36 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+import 'package:sizer/sizer.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'app/Services/graphql_conf.dart';
 import 'app/routes/app_pages.dart';
 
-void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
+GraphQLConfiguration graphQLConfiguration = GraphQLConfiguration();
+void main() async {
+  runApp(App());
+}
+
+Sizer App() {
+  return Sizer(
+    builder:
+        (BuildContext context, Orientation orientation, DeviceType deviceType) {
+      return GraphQLProvider(
+        client: graphQLConfiguration.client,
+        child: CacheProvider(
+          child: GetMaterialApp(
+            ///to take system theme
+            themeMode: ThemeMode.light,
+            title: "BellBoy",
+            initialRoute: AppPages.INITIAL,
+            debugShowCheckedModeBanner: false,
+            getPages: AppPages.routes,
+            defaultTransition: Transition.native,
+          ),
+        ),
+      );
+    },
   );
 }
