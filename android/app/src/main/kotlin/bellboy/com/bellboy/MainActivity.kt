7b746797c0f7564c9536a2418f7b3b4cd48729bd
binimuse <bini.musema@gmail.com>
2023-08-21 15:52:32 +0300

    first commit

---
+++ android/app/src/main/kotlin/bellboy/com/bellboy/MainActivity.kt
@@ -0,0 +1,6 @@
+package bellboy.com.bellboy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/config/theme/app_colors.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // /Primary
+  static const Color primary = Color(0xfff3971a);
+  static const Color primaryBlack = Color(0xffe07919);
+  static const Color primarylight = Color(0xffffce8b);
+  static const Color primarylighter = Color(0xffffefda);
+//Accent
+  static const Color accentdefault = Color(0xff784cd6);
+  static const Color accentdark = Color(0xff4a1a88);
+  static const Color accentlight = Color(0xffa48ee4);
+  static const Color accentlighter = Color(0xffebe1ff);
+//Danger
+  static const Color dangerdefault = Color(0xffe76565);
+  static const Color dangerdark = Color(0xffa92e2e);
+  static const Color dangerlight = Color(0xffffeaea);
+//Warning
+  static const Color warningdefault = Color(0xffffd02c);
+  static const Color warningdark = Color(0xffc28000);
+  static const Color warninglight = Color(0xfffff8d0);
+//Success
+  static const Color successdefault = Color(0xff1fa363);
+  static const Color successdark = Color(0xff1fa363);
+  static const Color successlight = Color(0xff1fa363);
+//Grayscale
+  static const Color grayscaleblack = Color(0xff171717);
+  static const Color grayscaleblacklight = Color(0xff3a3a3c);
+  static const Color grayscalegraydark = Color(0xff5d6066);
+  static const Color grayscaleDefault = Color(0xff808489);
+  static const Color grayscalegraylight = Color(0xffbcbec2);
+  static const Color grayscalegraylighter = Color(0xffd2d2d2);
+
+//Background
+  static const Color backgroundDark = Color(0xffe8e8e8);
+  static const Color backgroundLight = Color(0xfff6f6f6);
+
+//ETC
+
+  static Color etcdim = const Color(0xff222628).withOpacity(30);
+  static Color toastmessagebackground = const Color(0xff32363a).withOpacity(80);
+
+  //White
+  static const Color whiteoff = Color(0xffffffff);
+  static Color white70 = const Color(0xffffffff).withOpacity(70);
+  static Color white50 = const Color(0xffffffff).withOpacity(50);
+  static Color white30 = const Color(0xffffffff).withOpacity(30);
+  static Color white10 = const Color(0xffffffff).withOpacity(10);
+}
