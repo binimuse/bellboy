611778d86bdb9d51cb12df1f11b37fbe2f96b19c
binimuse <bini.musema@gmail.com>
2023-09-21 16:51:19 +0300

    some bug fixed on terms page

--- lib/app/modules/find_password/views/find_password_view.dart
+++ lib/app/modules/find_password/views/find_password_view.dart
@@ -1,7 +1,13 @@
+import 'package:bellboy/app/common/widgets/forms/text_input.dart';
+import 'package:bellboy/app/common/widgets/navigation/top_nav_back_text.dart';
+import 'package:bellboy/app/config/theme/app_colors.dart';
+import 'package:bellboy/app/config/theme/app_sizes.dart';
+import 'package:bellboy/app/config/theme/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../common/widgets/buttons/button_primary_fill.dart';
 import '../controllers/find_password_controller.dart';
 
 class FindPasswordView extends GetView<FindPasswordController> {
@@ -9,14 +15,70 @@ class FindPasswordView extends GetView<FindPasswordController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('FindPasswordView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'FindPasswordView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ///APP BAR
+            const TopNavBackText(
+              centerTitle: '',
+              rightText: '',
+              useHomeIcon: false,
+            ),
+
+            SizedBox(
+              height: AppSizes.mp_v_2,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: AppSizes.mp_w_4),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Find your password",
+                    textAlign: TextAlign.start,
+                    style: AppTextStyles.displayOneBold,
+                  ),
+                  SizedBox(
+                    height: AppSizes.mp_v_4,
+                  ),
+                  TextInput(
+                    hint: 'Email',
+                  ),
+                  SizedBox(
+                    height: AppSizes.mp_v_4,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: AppSizes.mp_w_8 * 2),
+                    child: Text(
+                      "Enter the e-mail address that you registered. Let us send your e-mail password reset link.",
+                      style: AppTextStyles.captionRegular
+                          .copyWith(color: AppColors.grayDark),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Expanded(
+              child: SizedBox(),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: AppSizes.mp_w_4),
+              child: Column(
+                children: [
+                  ButtonPrimaryFill(
+                    buttonSizeType: ButtonSizeType.LARGE,
+                    isDisabled: false,
+                    text: "Enter your E-mail",
+                    onTap: () {},
+                  ),
+                  SizedBox(
+                    height: AppSizes.mp_v_2,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
